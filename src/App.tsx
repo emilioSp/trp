b606ff2ce2a7bbaa2e5d30ab02fd75e6c67c0ffa
emiliosp <emiliosp.me@gmail.com>
2025-12-28 10:41:00 +0100

    feat: hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import MemberDetail from './pages/MemberDetail';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/trp" element={<Home />} />
-      <Route path="/member/:id" element={<MemberDetail />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trp" element={<Home />} />
+        <Route path="/member/:id" element={<MemberDetail />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
